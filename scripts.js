b281bda0678b1c07970992874a5a16c91154a425
Vitaly Hutarau <vitaliken@gmail.com>
2023-03-09 23:34:56 +0300

    add play/pause/skip functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
-//q
+//получение элементов
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//функции
+function togglePlay() {
+    if (video.paused) {
+        video.play()
+    } else {
+        video.pause()
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? "▶" : "𝅛𝅛";
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+
+
+
+//события
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
