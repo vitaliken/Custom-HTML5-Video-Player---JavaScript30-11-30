d54c733f408694a6e4ce4b4a91234482c55a1d61
Vitaly Hutarau <vitaliken@gmail.com>
2023-03-10 17:35:15 +0300

    add timeupdate

--- scripts.js
+++ scripts.js
@@ -22,12 +22,25 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    // console.log(this.name);
+    // console.log(this.value);
+}
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 
 //события
 video.addEventListener('click', togglePlay);
@@ -35,5 +48,19 @@ toggle.addEventListener('click', togglePlay);
 
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousemove', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+
+
+
+
